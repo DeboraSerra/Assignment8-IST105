5917555a3be28d2e83c4f45bddd095370032c789
Debora Serra <debora.r.serra@gmail.com>
2025-03-19 19:58:14 -0700

    chore: update host ip to correct value

--- process.php
+++ process.php
@@ -6,7 +6,7 @@
   $command = escapeshellcmd("python3 network_config.py $mac_address $dhcp");
   $output = shell_exec($command);
 
-  $host_ip = $_SERVER['HTTP_HOST'];
+  $host_ip = $_SERVER['SERVER_ADDR'];
 
   echo "<h1 style='text-align: center; margin: 32px 0;'>Network Configuration</h1>";
   echo "Current IP address: $host_ip<br>";
