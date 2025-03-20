33beaa4c9ce0216f21d090a41d323ba1a2dab220
Debora Serra <debora.r.serra@gmail.com>
2025-03-19 19:43:32 -0700

    feat: add host ip

--- process.php
+++ process.php
@@ -6,6 +6,11 @@
   $command = escapeshellcmd("python3 network_config.py $mac_address $dhcp");
   $output = shell_exec($command);
 
+  $host_ip = $_SERVER['HTTP_HOST'];
+
+  echo "<h1 style='text-align: center; margin: 32px 0;'>Network Configuration</h1>";
+  echo "Current IP address: $host_ip<br>";
+
   echo $output;
   
 ?>
